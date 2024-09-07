b3fef533e027d4f925bdbdf566f5fd4c1ce4662a
Chandan <chandansahoo02468@gmail.com>
2024-09-08 01:19:08 +0530

    add user controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,10 +1,79 @@
-import {asyncHandler} from "../utils/asyncHandler.js"
+import { asyncHandler } from "../utils/asyncHandler.js";
+import { userType } from "../types.js";
+import { apiError } from "../utils/apiError.js";
+import { User } from "../models/user.model.js";
+import { uploadOnCloudinary } from "../utils/cloudinary.js";
+import { apiResponse } from "../utils/apiResponse.js";
+const registerUser = asyncHandler(async (req, res) => {
+  // Get user details from frontend
+  const createPayload = req.body;
+  const parsePayload = userType.safeParse(createPayload);
 
+  if (!parsePayload.success) {
+    const usernameError = parsePayload.error.formErrors.fieldErrors.username;
+    const emailError = parsePayload.error.formErrors.fieldErrors.email;
+    const fullNameError = parsePayload.error.formErrors.fieldErrors.fullname;
+    const passwordError = parsePayload.error.formErrors.fieldErrors.password;
 
-const registerUser = asyncHandler(async(req,res)=>{
-    res.status(200).json({
-        message : "ok"
-    })
-})
+    if (usernameError) throw new apiError(400, usernameError[0]);
+    if (emailError) throw new apiError(400, emailError[0]);
+    if (fullNameError) throw new apiError(400, fullNameError[0]);
+    if (passwordError) throw new apiError(400, passwordError[0]);
+  }
 
-export {registerUser}
+  const { username, email, fullName, avatar, password } = createPayload;
+
+  const existedUser = await User.findOne({
+    $or: [{ username }, { email }],
+  });
+
+  if (existedUser) {
+    if (existedUser.username === username && existedUser.email === email) {
+      throw new apiError(
+        409,
+        "User with the same username and email already exists."
+      );
+    } else if (existedUser.email === email) {
+      throw new apiError(409, "Email is already registered.");
+    } else if (existedUser.username === username) {
+      throw new apiError(409, "Username is already taken.");
+    }
+  }
+
+  const avatarLocalPath = req.files?.avatar?.[0]?.path;
+  const coverImageLocalPath = req.files?.coverImage?.[0]?.path;
+
+  if (!avatarLocalPath) {
+    throw new apiError(400, "Avatar file is required.");
+  }
+
+  const avatarUrl = await uploadOnCloudinary(avatarLocalPath);
+  const coverImageUrl = await uploadOnCloudinary(coverImageLocalPath);
+
+  if(!avatarUrl){
+    throw new apiError(400, "Avatar file is required")
+  }
+  
+  const newUser = await User.create({
+    username : username.toLowerCase(),
+    email,
+    fullName,
+    password,
+    avatar: avatarUrl.url,
+    coverImage: coverImageLocalPath?.url || "",
+  });
+
+  const createdNewUser = User.findById(newUser._id).select(
+    "-password -refreshToken"
+  )
+
+  if(!createdNewUser){
+    throw new apiError(500,"Something went wrong while registering the user")
+  }
+
+  res.status(201).json(
+    new apiResponse(200,createdNewUser,"User registered successfully")
+  );
+});
+
+export { registerUser };
---
+++ src/types.js
@@ -0,0 +1,12 @@
+import zod from "zod"
+
+const userType = zod.object({
+    username: zod.string().min(1,{message : "Please enter a valid username"}),
+    email : zod.string().min(1,{message : "Please enter a valid email"}),
+    fullName : zod.string().min(1,{message : "Please enter a valid fullname"}),
+    avatar : zod.optional(),
+    coverImage : zod.optional(),
+    password  :zod.string().min(1,{message : "Please enter a valid password"}),
+})
+
+export {userType}
